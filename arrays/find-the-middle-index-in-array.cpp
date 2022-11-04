e6461512b1c6037dcfd891025fc6c0ef5aad40d6
VS <varkisebi@gmail.com>
2022-11-04 14:13:37 +0530

    arrays-ii

---
+++ arrays/find-the-middle-index-in-array.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/find-the-middle-index-in-array/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findMiddleIndex(vector<int>& nums) {
+    int sum=0, lSum=0, n=nums.size();
+    
+    for(int i=0; i<n; i++) sum+=nums[i];
+    for(int i=0; i<n; i++) {
+        lSum+=nums[i];
+        if(lSum == sum) return i;
+        sum-=nums[i];
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> nums({2,3,-1,8,4});
+    int result = findMiddleIndex(nums);
+    cout << result;
+}
