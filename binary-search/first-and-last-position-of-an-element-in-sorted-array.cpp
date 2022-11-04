806838ff3031490bd3c25c5e7faab58acf41993c
VS <varkisebi@gmail.com>
2022-11-04 12:58:52 +0530

    binary-search-ii

--- binary-search/first-and-last-position-of-an-element-in-sorted-array.cpp
+++ binary-search/first-and-last-position-of-an-element-in-sorted-array.cpp
@@ -1,40 +1,32 @@
 // https://www.codingninjas.com/codestudio/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
 #include<vector>
+#include<iostream>
 using namespace std;
 
-int firstOccurrence (vector<int>& arr, int n, int k) {
-    int start=0, end=n-1;
-    int mid = start + (end-start)/2;
-    int firstIndex = -1;
-    
+int firstOccurrence(vector <int>arr, int n, int k){
+    int start=0, end=n, ans=-1;
     while(start<=end) {
-        if(k == arr[mid]) {
-            firstIndex = mid;
-            end = mid-1;
-        } else if(k < arr[mid]) end = mid-1;
-        else start = mid+1;
-
-        mid = start + (end-start)/2;
+        int mid=start+(end-start)/2;
+        if(arr[mid] == k) {
+            ans=mid;
+            end=mid-1;
+        } else if(arr[mid]>k) end=mid-1;
+        else start=mid+1;
     }
-    return firstIndex;
+    return ans;
 }
 
-int lastOccurrence (vector<int>& arr, int n, int k) {
-    int start=0, end=n-1;
-    int mid = start + (end-start/2);
-    int lastIndex = -1;
-
+int lastOccurrence(vector <int>arr, int n, int k){
+    int start=0, end=n, ans=-1;
     while(start<=end) {
+        int mid=start+(end-start)/2;
         if(arr[mid] == k) {
-            lastIndex = mid;
-            start = mid+1;
-        } else if(k < arr[mid]) end = mid-1;
-        else start = mid+1;
-
-        mid = start + (end-start)/2;
+            ans=mid;
+            start=mid+1;
+        } else if(arr[mid]>k) end=mid-1;
+        else start=mid+1;
     }
-    
-    return lastIndex;
+    return ans;
 }
 
 pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
@@ -44,4 +36,15 @@ pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
     p.second = lastOccurrence(arr, n, k);
 
     return p;
+}
+
+int main() {
+    vector<int> arr({1, 2, 2, 2, 2, 3, 4, 7, 8, 8 });
+    int n=10, k=8;
+    pair<int,int>result = firstAndLastPosition(arr, n, k);
+    cout<<result.first << " " << result.second << endl;
+
+    // program to find the frequency of an element
+    int frequency = result.second-result.first+1;
+    cout<< frequency;
 }
