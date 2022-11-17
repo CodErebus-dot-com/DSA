ffd9b3e8c621dea486122dd49501fac0444f03a7
VS <varkisebi@gmail.com>
2022-11-18 02:52:55 +0530

    strings-iii

---
+++ 05-strings/05-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+#include <iostream>
+using namespace std;
+
+string removeOccurrences(string s, string part) {
+    while(s.length()!=0 && s.find(part)!=string::npos) s.erase(s.find(part), part.length());
+    return s; 
+}
+
+int main() {
+    string s="daabcbaabcbc", part="abc";
+    cout<<removeOccurrences(s, part);
+}
